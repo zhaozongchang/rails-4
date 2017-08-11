b34ad8176339d424584f773ba3d4469ac77824b5
zhaozongchang <xingyingsuifeng@gmail.com>
2017-08-11 17:44:37 +0800

    add validation ro title apply ro edit/update

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -28,10 +28,13 @@ class GroupsController < ApplicationController
      def update
        @group = Group.find(params[:id])
 
-       @group.update(group_params)
+       if @group.update(group_params)
 
        redirect_to groups_path, notice: "更新成功"
+     else
+       render :edit
      end
+    end
 
      def destroy
        @group = Group.find(params[:id])
