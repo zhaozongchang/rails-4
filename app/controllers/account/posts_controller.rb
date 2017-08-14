0bee241aa09d6f9a6625b6c1f7e0002c508656b1
zhaozongchang <xingyingsuifeng@gmail.com>
2017-08-14 16:20:05 +0800

    implement edit destroy

--- app/controllers/account/posts_controller.rb
+++ app/controllers/account/posts_controller.rb
@@ -3,4 +3,25 @@ class Account::PostsController < ApplicationController
     def index
       @posts = current_user.posts
     end
+
+    def show
+      @posts = Posts.find(params[:id])
+
+    def edit
+      @posts = Posts.find(params[:id])
+    end
+
+    def update
+      @gosts = Gosts.find(params[:group_id])
+      @posts = Posts.find(params[:id])
+      redirect_to posts_path,notice:"更新成功..."
+    end
+
+    def destroy
+      @posts = Posts.find(params[:id])
+      @posts.destroy
+      flash[:alert] = "Group deleted"
+      redirect_to posts_path
+    end
+   end
   end
